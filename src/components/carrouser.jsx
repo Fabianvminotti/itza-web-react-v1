97af6b2a3fee8dc23c39bcc13cc574719422a8f2
Fabian Minotti <fabianvminotti@gmail.com>
2023-01-11 16:09:49 -0300

    arreglo del carrusel

--- src/components/carrouser.jsx
+++ src/components/carrouser.jsx
@@ -6,30 +6,32 @@ export const Carrousel = (props) => {
     
     console.log(props)
     return (
-<div style={{ padding: '60px' }}>
-<div className='section-title'>
-          <h2>Nuestro trabajo</h2>
-          
+        <div className="d-flex justify-content-center" style={{ padding: '60px' }}>
+          <div className='text-center' style={{ width: "60%", minWidth: "400px" }}>
+            <div className='section-title ' id='portfolio'>
+              <h2>Nuestro trabajo</h2>
+            </div>
+            <Carousel>
+              <div>
+                <img src='img/galeria1000x1000/IMG_20210803_210722_815.jpg' />
+                {<p className="legend">Preparacion de relevamiento con VANT</p>}
+              </div>
+              <div>
+                <img src='img/galeria1000x1000/IMG_20210911_161148.jpg' />
+                {<p className="legend">Relevamiento topografico con VANT en la localidad de Aldea Brasilera</p>}
+              </div>
+              <div>
+                <img src='img/galeria1000x1000/IMG_20211127_173814.jpg' />
+                {<p className="legend">Uno de los equipos utilizados</p>}
+              </div>
+              <div>
+                <img src='img/galeria1000x1000/IMG_20220107_093039.jpg' />
+                {<p className="legend">Colocacion de punto de apoyo fotogrametrico (PAF)</p>}
+              </div>
+            </Carousel>
+          </div>
         </div>
-<Carousel>
-            
-                    
-            <div>
-                            <img src='img/team/nacho.png' />
-                            {<p className="legend">Ignacio olguin</p>}
-                        </div>
-                        <div>
-                            <img src='img/team/mino.png' />
-                            {<p className="legend">Fabian Minotti</p>}
-                        </div>
-                   
-
-        </Carousel>
-
-</div>
-
-        
-    );
+      );
 }
 
 //export default Carrousel
