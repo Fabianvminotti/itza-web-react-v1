42a0b7366f034c4e2d482f48891ae054bc32ae6b
Fabian Minotti <fabianvminotti@gmail.com>
2023-01-24 22:04:06 -0300

    pequenas mejoras

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -5,8 +5,7 @@ export const Team = (props) => {
         <div className='col-md-8 col-md-offset-2 section-title'>
           <h2>Nosotros</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            Estas somos algunas de las personas que comformamos ITZA
           </p>
         </div>
         <div id='rowTeam' >
