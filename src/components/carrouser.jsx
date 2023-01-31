88fa5e7f88ce6cb2dfb7fa8945ee3893ac4a42a3
Fabian Minotti <fabianvminotti@gmail.com>
2023-01-31 16:57:45 -0300

    version final 1

--- src/components/carrouser.jsx
+++ src/components/carrouser.jsx
@@ -15,10 +15,20 @@ export const Carrousel = (props) => {
               <h2>Nuestro trabajo</h2>
             </div>
             <Carousel>
-              <div>
+              {/* <div>
                 <img src='img/galeria1000x1000/IMG_20210803_210722_815.jpg' />
                 {<p className="legend">Preparacion de relevamiento con VANT</p>}
+              </div> */}
+              {/* <div>
+                <img src='img/joa_drone.jpeg' />
+                {<p className="legend">Preparación de equipos para un vuelo fotogrametrico</p>}
+              </div> */}
+             
+              <div>
+                <img src='img/galeria1000x1000/566.jpg' />
+                {<p className="legend">Preparación de equipos para un vuelo fotogrametrico</p>}
               </div>
+             
               <div>
                 <img src='img/galeria1000x1000/IMG_20210911_161148.jpg' />
                 {<p className="legend">Relevamiento topografico con VANT en la localidad de Aldea Brasilera, Entre Rios</p>}
@@ -26,6 +36,11 @@ export const Carrousel = (props) => {
               <div>
                 <img src='img/galeria1000x1000/IMG_20211127_173814.jpg' />
                 {<p className="legend">Uno de los equipos utilizados</p>}
+              </div>
+              
+              <div>
+                <img src='img/galeria1000x1000/234.jpg' />
+                {<p className="legend">Nuestra participacoon en el programa Capital Activa de la Municipalidad de Santa Fe</p>}
               </div>
               <div>
                 <img src='img/galeria1000x1000/IMG_20220107_093039.jpg' />
